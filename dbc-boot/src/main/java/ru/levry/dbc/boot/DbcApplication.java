7c912cb062ee3fc862e14729851f8554f48ed143
levry <lriskov@gmail.com>
2017-05-23 12:25:37 +0500

    Boot as war

--- dbc-boot/src/main/java/ru/levry/dbc/boot/DbcApplication.java
+++ dbc-boot/src/main/java/ru/levry/dbc/boot/DbcApplication.java
@@ -2,12 +2,20 @@ package ru.levry.dbc.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author levry
  */
 @SpringBootApplication
-public class DbcApplication {
+public class DbcApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DbcApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DbcApplication.class, args);
     }
